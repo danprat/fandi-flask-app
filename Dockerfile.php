44d8fcb507cafd0efffdab11cc54b2d6b7ce21f1
Dany Pratmanto <danybms@gmail.com>
2025-09-19 08:12:34 +0700

    Fix Apache opcache issue and ServerName; make python container handle missing tes.py gracefully

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,6 +3,13 @@ FROM php:8.1-apache
 # Install mysqli extension
 RUN docker-php-ext-install mysqli
 
+# Disable opcache via ini (helps avoid binary mismatch errors)
+RUN printf "opcache.enable=0\nopcache.enable_cli=0\n" > /usr/local/etc/php/conf.d/disable-opcache.ini
+
+# Set Apache ServerName to suppress AH00558
+RUN printf "ServerName localhost\n" > /etc/apache2/conf-available/servername.conf \
+    && a2enconf servername
+
 # Copy PHP files to web root
 COPY . /var/www/html/
 
